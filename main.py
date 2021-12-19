5e25883f7b09f9a053c0373590a008c3e427c11c
Chiraagkv <69423507+Chiraagkv@users.noreply.github.com>
2021-12-19 11:33:24 +0530

    add break for loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import cv2 
 import pyttsx3
+import keyboard
 
 # load the COCO class labels our YOLO model was trained on
 LABELS = open("coco.names").read().strip().split("\n")
@@ -9,6 +10,7 @@ LABELS = open("coco.names").read().strip().split("\n")
 # load our YOLO object detector trained on COCO dataset (80 classes)
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet("yolov3.cfg", "yolov3.weights")
+print("Done!")
 
 # determine only the *output* layer names that we need from YOLO
 ln = net.getLayerNames()
@@ -27,66 +29,69 @@ def say(out_say):
 
 while True:
 	frame_count += 1
-    # Capture frame-by-frameq
-	ret, frame = cap.read()
-	frame = cv2.flip(frame, 1)
-	frames.append(frame)
+	if keyboard.is_pressed("q"):
+		break
+	else:
+	    # Capture frame-by-frame
+		ret, frame = cap.read()
+		frame = cv2.flip(frame, 1)
+		frames.append(frame)
 
-	if ret:
-		cv2.imshow('Video Feed', cv2.flip(frame, 1))
-		key = cv2.waitKey(1)
-		if not frame_count % 100: # runs predictor once every 100 frames
-			end = time.time()
-			(H, W) = frame.shape[:2]
-			blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-				swapRB=True, crop=False)
-			net.setInput(blob)
-			layerOutputs = net.forward(ln)
-			boxes = []
-			confidences = []
-			classIDs = []
-			centers = []
-			for output in layerOutputs:
-				for detection in output:
-					scores = detection[5:]
-					classID = np.argmax(scores)
-					confidence = scores[classID]
-					if confidence > 0.5:
-						box = detection[0:4] * np.array([W, H, W, H])
-						(centerX, centerY, width, height) = box.astype("int")
-						x = int(centerX - (width / 2))
-						y = int(centerY - (height / 2))
-						boxes.append([x, y, int(width), int(height)])
-						confidences.append(float(confidence))
-						classIDs.append(classID)
-						centers.append((centerX, centerY))
-			idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+		if ret:
+			cv2.imshow('Video Feed', cv2.flip(frame, 1))
+			key = cv2.waitKey(1)
+			if not frame_count % 100: # runs predictor once every 100 frames
+				end = time.time()
+				(H, W) = frame.shape[:2]
+				blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+					swapRB=True, crop=False)
+				net.setInput(blob)
+				layerOutputs = net.forward(ln)
+				boxes = []
+				confidences = []
+				classIDs = []
+				centers = []
+				for output in layerOutputs:
+					for detection in output:
+						scores = detection[5:]
+						classID = np.argmax(scores)
+						confidence = scores[classID]
+						if confidence > 0.5:
+							box = detection[0:4] * np.array([W, H, W, H])
+							(centerX, centerY, width, height) = box.astype("int")
+							x = int(centerX - (width / 2))
+							y = int(centerY - (height / 2))
+							boxes.append([x, y, int(width), int(height)])
+							confidences.append(float(confidence))
+							classIDs.append(classID)
+							centers.append((centerX, centerY))
+				idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
 
-			texts = []
-			if len(idxs) > 0:
-				for i in idxs.flatten():
-					centerX, centerY = centers[i][0], centers[i][1]
-					
-					if centerX <= W/3:
-						W_pos = "left "
-					elif centerX <= (W/3 * 2):
-						W_pos = "center "
-					else:
-						W_pos = "right "
-					
-					if centerY <= H/3:
-						H_pos = "top "
-					elif centerY <= (H/3 * 2):
-						H_pos = "mid "
-					else:
-						H_pos = "bottom "
+				texts = []
+				if len(idxs) > 0:
+					for i in idxs.flatten():
+						centerX, centerY = centers[i][0], centers[i][1]
 
-					texts.append(H_pos + W_pos + LABELS[classIDs[i]])
+						if centerX <= W/3:
+							W_pos = "left "
+						elif centerX <= (W/3 * 2):
+							W_pos = "center "
+						else:
+							W_pos = "right "
 
-			
-			if texts:
-				description = ', '.join(texts)
-				say(description)
+						if centerY <= H/3:
+							H_pos = "top "
+						elif centerY <= (H/3 * 2):
+							H_pos = "mid "
+						else:
+							H_pos = "bottom "
+
+						texts.append(H_pos + W_pos + LABELS[classIDs[i]])
+
+
+				if texts:
+					description = ', '.join(texts)
+					say(description)
 
 cap.release()
 cv2.destroyAllWindows()
