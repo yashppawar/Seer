90c1a4cf6312f454e1409932b797a6c6f8371f5d
Yash Pawar <yashpawarp@gmail.com>
2021-12-21 05:01:09 +0530

    cleanup code

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ frame_count = 0
 start = time.time()
 first = True
 frames = []
+
 def say(out_say):
     engine = pyttsx3.init()
     engine.say(out_say)
@@ -29,69 +30,91 @@ def say(out_say):
 
 while True:
 	frame_count += 1
+
 	if keyboard.is_pressed("q"):
 		break
-	else:
-	    # Capture frame-by-frame
-		ret, frame = cap.read()
-		frame = cv2.flip(frame, 1)
-		frames.append(frame)
+	
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	frame = cv2.flip(frame, 1)
+	frames.append(frame)
 
-		if ret:
-			cv2.imshow('Video Feed', cv2.flip(frame, 1))
-			key = cv2.waitKey(1)
-			if not frame_count % 100: # runs predictor once every 100 frames
-				end = time.time()
-				(H, W) = frame.shape[:2]
-				blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-					swapRB=True, crop=False)
-				net.setInput(blob)
-				layerOutputs = net.forward(ln)
-				boxes = []
-				confidences = []
-				classIDs = []
-				centers = []
-				for output in layerOutputs:
-					for detection in output:
-						scores = detection[5:]
-						classID = np.argmax(scores)
-						confidence = scores[classID]
-						if confidence > 0.5:
-							box = detection[0:4] * np.array([W, H, W, H])
-							(centerX, centerY, width, height) = box.astype("int")
-							x = int(centerX - (width / 2))
-							y = int(centerY - (height / 2))
-							boxes.append([x, y, int(width), int(height)])
-							confidences.append(float(confidence))
-							classIDs.append(classID)
-							centers.append((centerX, centerY))
-				idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+	if not ret: continue # if no frame, go to next iteration
 
-				texts = []
-				if len(idxs) > 0:
-					for i in idxs.flatten():
-						centerX, centerY = centers[i][0], centers[i][1]
+	cv2.imshow('Video Feed', cv2.flip(frame, 1))
+	key = cv2.waitKey(1)
 
-						if centerX <= W/3:
-							W_pos = "left "
-						elif centerX <= (W/3 * 2):
-							W_pos = "center "
-						else:
-							W_pos = "right "
+	if frame_count % 100: # runs predictor once every 100 frames
+		continue  # guard, if its not 100th frame, skip the loop
 
-						if centerY <= H/3:
-							H_pos = "top "
-						elif centerY <= (H/3 * 2):
-							H_pos = "mid "
-						else:
-							H_pos = "bottom "
+	end = time.time()
 
-						texts.append(H_pos + W_pos + LABELS[classIDs[i]])
+	(H, W) = frame.shape[:2]
+	
+	blob = cv2.dnn.blobFromImage(
+		frame, 
+		1 / 255.0, 
+		(416, 416),
+		swapRB = True, 
+		crop = False
+	)
+
+	net.setInput(blob)
+	layerOutputs = net.forward(ln)
+	
+	boxes = []
+	confidences = []
+	classIDs = []
+	centers = []
+
+	for output in layerOutputs:
+		for detection in output:
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+
+			if confidence > 0.5:
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+				
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+				
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+				centers.append((centerX, centerY))
+
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+
+	texts = []
+
+	if len(idxs) <= 0:  # no idxs so dont run the next code
+		continue
+
+	for i in idxs.flatten():
+		centerX, centerY = centers[i]
+
+		if centerX <= W/3:
+			W_pos = "left "
+		elif centerX <= (W/3 * 2):
+			W_pos = "center "
+		else:
+			W_pos = "right "
+
+		if centerY <= H/3:
+			H_pos = "top "
+		elif centerY <= (H/3 * 2):
+			H_pos = "mid "
+		else:
+			H_pos = "bottom "
+
+		texts.append(H_pos + W_pos + LABELS[classIDs[i]])
 
 
-				if texts:
-					description = ', '.join(texts)
-					say(description)
+	if texts:
+		description = ', '.join(texts)
+		say(description)
 
 cap.release()
 cv2.destroyAllWindows()
